91d405b5c9c74b821f7bfd901732c2bbc410b022
willys_tjiawi <willys@huubap.com>
2018-12-18 01:35:45 +0900

    @babel/polyfill

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,8 @@ import '../style/css/main.css';
 import '../style/scss/main.scss';
 import {hello, arr} from './module';
 import {inlinesourcemapFn} from './inlinesourcemap';
+import '@babel/polyfill';
+
 
 console.log('this is index.js!!!!!');
 
